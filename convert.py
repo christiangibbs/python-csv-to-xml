fd9d7c7a80f15f785b57ac6a45ab6d5a4c2ac958
Christian Gibbs <christian@cjgsolutions.com>
2015-10-07 16:24:22 +0100

    Removed dependency for input.csv and limit of 1, new update looks for all files in directory and processes them all individually regardless of name (*.csv)

--- convert.py
+++ convert.py
@@ -2,6 +2,9 @@
 # Christian J. Gibbs - 05/10/2015
 # The first row of the .csv file must be headers
 
+import os, os.path
+import glob
+import shutil
 import csv
 import datetime
 now = datetime.datetime.now()
@@ -15,56 +18,78 @@ minute = str(now.minute)
 second = str(now.second)
 microsecond = str(now.microsecond)
 
-# Take input.csv and set the file output to have a unique name
-csvFile = 'raw/input.csv'
-xmlFile = 'complete/13088' + day + month + year + hour + minute + second + '.xml'
+# Check for files in /raw folder
+files = glob.glob("raw/*.csv")
+file_count = len(files)
+dir = "raw/"
 
-# Open the csv file to read
-csvData = csv.reader(open(csvFile))
+for i in range(len(files)):
+    
+    # Take input.csv and set the file output to have a unique name
+    csvFile = files[i]
+    xmlFile = 'complete/13088' + day + month + year + hour + minute + second + str(i) + '.xml'
+    
+    # progress = (float(i)/float(file_count))*100
+    
+    # Remove directorry
+    file = csvFile.strip(dir)
+    
+    # Open the csv file to read
+    csvData = csv.reader(open(csvFile))
+    
+    # Open xml file created earlier and write a header
+    xmlData = open(xmlFile, 'w')
+    xmlData.write('<?xml version="1.0" encoding="ISO-8859-1" ?>' + "\n")
+    xmlData.write('<Orders>' + "\n")
+    xmlData.write('<Order>' + "\n")
+    xmlData.write('<Order_Date><![CDATA[' + day + '/' + month +'/' + year + ']]></Order_Date>' + "\n")
+    
+    # Add your order ID format - example. ID-00000
+    xmlData.write('<Order_ID><![CDATA[DT-13088' + day + month + year + hour + minute + second + str(i) + ']]></Order_ID>' + "\n")
+    
+    # Add your customer ID - example. ID000
+    xmlData.write('<Customer_ID><![CDATA[DEPN000]]></Customer_ID>' + "\n")
+    xmlData.write('<Products>' + "\n")
+    
+    
+    
+    
+    # Parse the csv file and write the styled information to an xml file
+    rowNum = 0
+    for row in csvData:
+        if rowNum == 0:
+            tags = row
+            for i in range(len(tags)):
+                tags[i] = tags[i].replace(' ', '_')
+                if tags[i] == '_SKU':
+                    tags[i] = tags[i].replace('_SKU', 'SKU')
+                else:
+                    tags[i] == tags[i]
+                
+                if tags[i] == '_Quantity_':
+                    tags[i] = tags[i].replace('_Quantity_', 'Quantity')
+                else:
+                    tags[i] == tags[i]
+    
+    
+        else:
+            xmlData.write('<Item>' + "\n")
+            for i in range(len(tags)):
+                xmlData.write('<' + tags[i] + '>' \
+                              + '<![CDATA[' + row[i] + ']]>' + '</' + tags[i] + '>' + "\n")
+            xmlData.write('</Item>' + "\n")
 
-# Open xml file created earlier and write a header
-xmlData = open(xmlFile, 'w')
-xmlData.write('<?xml version="1.0" encoding="ISO-8859-1" ?>' + "\n")
-xmlData.write('<Orders>' + "\n")
-xmlData.write('<Order>' + "\n")
-xmlData.write('<Order_Date><![CDATA[' + day + '/' + month +'/' + year + ']]></Order_Date>' + "\n")
-
-# Add your order ID format - example. ID-00000
-xmlData.write('<Order_ID><![CDATA[DT-13088' + day + month + year + hour + minute + second + ']]></Order_ID>' + "\n")
-
-# Add your customer ID - example. ID000
-xmlData.write('<Customer_ID><![CDATA[DEPN000]]></Customer_ID>' + "\n")
-xmlData.write('<Products>' + "\n")
-
-# Parse the csv file and write the styled information to an xml file
-rowNum = 0
-for row in csvData:
-    if rowNum == 0:
-        tags = row
-        for i in range(len(tags)):
-            tags[i] = tags[i].replace(' ', '_')
-            if tags[i] == '_SKU':
-                tags[i] = tags[i].replace('_SKU', 'SKU')
-            else:
-                tags[i] == tags[i]
-
-            if tags[i] == '_Quantity_':
-                tags[i] = tags[i].replace('_Quantity_', 'Quantity')
-            else:
-                tags[i] == tags[i]
-
-
-    else:
-        xmlData.write('<Item>' + "\n")
-        for i in range(len(tags)):
-            xmlData.write('<' + tags[i] + '>' \
-                          + '<![CDATA[' + row[i] + ']]>' + '</' + tags[i] + '>' + "\n")
-        xmlData.write('</Item>' + "\n")
-
-    rowNum +=1
-
-xmlData.write('</Products>' + "\n")
-xmlData.write('</Order>' + "\n")
-xmlData.write('</Orders>' + "\n")
-
-xmlData.close()
+rowNum +=1
+    
+    xmlData.write('</Products>' + "\n")
+    xmlData.write('</Order>' + "\n")
+    xmlData.write('</Orders>' + "\n")
+    
+    xmlData.close()
+    
+    #print('Complete: ' + file)
+    # print('Progress: ' + str(progress) + '%')
+    
+    # Move file to other folder when processing automatically - avoids duplicate processing
+    shutil.move("raw/" + file, "processed/" + file)
+# print('Progress: 100%')
