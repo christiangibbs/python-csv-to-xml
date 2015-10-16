caa597016268ad6e16bbfb4e26d12a04ef8ca364
Christian Gibbs <christiangibbs@Christians-iMac.local>
2015-10-16 13:18:31 +0100

    file management added and setup.py

--- convert.py
+++ convert.py
@@ -24,7 +24,7 @@ file_count = len(files)
 dir = "raw/"
 
 for i in range(len(files)):
-    
+
     # Take input.csv and set the file output to have a unique name
     csvFile = files[i]
     xmlFile = 'complete/13088' + day + month + year + hour + minute + second + str(i) + '.xml'
@@ -33,26 +33,26 @@ for i in range(len(files)):
     
     # Remove directorry
     file = csvFile.strip(dir)
-    
+
     # Open the csv file to read
     csvData = csv.reader(open(csvFile))
-    
+
     # Open xml file created earlier and write a header
     xmlData = open(xmlFile, 'w')
     xmlData.write('<?xml version="1.0" encoding="ISO-8859-1" ?>' + "\n")
     xmlData.write('<Orders>' + "\n")
     xmlData.write('<Order>' + "\n")
     xmlData.write('<Order_Date><![CDATA[' + day + '/' + month +'/' + year + ']]></Order_Date>' + "\n")
-    
+
     # Add your order ID format - example. ID-00000
     xmlData.write('<Order_ID><![CDATA[DT-13088' + day + month + year + hour + minute + second + str(i) + ']]></Order_ID>' + "\n")
-    
+
     # Add your customer ID - example. ID000
     xmlData.write('<Customer_ID><![CDATA[DEPN000]]></Customer_ID>' + "\n")
     xmlData.write('<Products>' + "\n")
-    
-    
-    
+
+
+
     
     # Parse the csv file and write the styled information to an xml file
     rowNum = 0
@@ -65,13 +65,13 @@ for i in range(len(files)):
                     tags[i] = tags[i].replace('_SKU', 'SKU')
                 else:
                     tags[i] == tags[i]
-                
+
                 if tags[i] == '_Quantity_':
                     tags[i] = tags[i].replace('_Quantity_', 'Quantity')
                 else:
                     tags[i] == tags[i]
-    
-    
+
+
         else:
             xmlData.write('<Item>' + "\n")
             for i in range(len(tags)):
@@ -79,17 +79,17 @@ for i in range(len(files)):
                               + '<![CDATA[' + row[i] + ']]>' + '</' + tags[i] + '>' + "\n")
             xmlData.write('</Item>' + "\n")
 
-rowNum +=1
-    
+        rowNum +=1
+
     xmlData.write('</Products>' + "\n")
     xmlData.write('</Order>' + "\n")
     xmlData.write('</Orders>' + "\n")
-    
+
     xmlData.close()
-    
-    #print('Complete: ' + file)
-    # print('Progress: ' + str(progress) + '%')
-    
+
+#print('Complete: ' + file)
+# print('Progress: ' + str(progress) + '%')
+
     # Move file to other folder when processing automatically - avoids duplicate processing
     shutil.move("raw/" + file, "processed/" + file)
 # print('Progress: 100%')
